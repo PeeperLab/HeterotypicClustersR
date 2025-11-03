c8dd18ef138a9cbdc2e21b5416a900185744c4c3
AustinGeorgeNKI <a.george@nki.nl>
2025-11-03 14:28:20 +0100

    Update s0_extract_curated_pairs.R
    updated with folders

--- Scripts/4.Nichenet/s0_extract_curated_pairs.R
+++ Scripts/4.Nichenet/s0_extract_curated_pairs.R
@@ -8,16 +8,19 @@
 library(data.table)
 #setwd("/YOUR/PATH")
 
+fig_path = "Results_Nichenet"
+if(!dir.exists(fig_path)) dir.create(fig_path)
+
 # # single cell signal R
-load("database_curation/data_LRdb.rda")
+load("extdata/database_curation/data_LRdb.rda")
 setDT(LRdb)
 LRdb[, lr_pair:= paste0(ligand,"_",receptor)]
 
 # # celltalk_db
-celltalk_db=fread("database_curation/human_lr_pair.txt")
+celltalk_db=fread("extdata/database_curation/human_lr_pair.txt")
 
 # # cell chat - PPI
-load("database_curation/PPI.human.rda")
+load("extdata/database_curation/PPI.human.rda")
 PPI.human=as.matrix(PPI.human) 
 PPI.human=data.frame(PPI.human)
 PPI.human$prot1=rownames(PPI.human)
@@ -30,7 +33,7 @@ PPI.human[, lr_pair:= paste0(prot1, "_",variable)]
 PPI.human[, ppi.hu:=1]
 
 # # cell chat - interactions
-load("database_curation/CellChatDB.human.rda")
+load("extdata/database_curation/CellChatDB.human.rda")
 cc_int=CellChatDB.human$interaction
 setDT(cc_int)
 
@@ -54,14 +57,14 @@ curated_receptors=union(union(cc_int$receptor, LRdb$receptor), celltalk_db$recep
 final_pairs=merged[to %in% curated_receptors ,]
 
 # add receptor localization info from uniprot
-uniprot_membrane=fread("database_curation/uniprotkb_cell_membrane_OR_surface_AND_2024_11_07.tsv")
+uniprot_membrane=fread("extdata/database_curation/uniprotkb_cell_membrane_OR_surface_AND_2024_11_07.tsv")
 setnames(uniprot_membrane,"Subcellular location [CC]","location")
 uniprot_membrane=uniprot_membrane[ grepl("surface", location)| grepl("membrane", location),]
 uniprot_membrane_genes=uniprot_membrane$`Gene Names`
 uniprot_membrane_genes=unique(unlist(stringr::str_split(uniprot_membrane_genes, " ")))
 
 final_pairs=final_pairs[to %in% uniprot_membrane_genes,]
-fwrite(final_pairs[,.(from, to, lr_pair)], "s0_potential_pairs.txt")
+fwrite(final_pairs[,.(from, to, lr_pair)], "Results_Nichenet/s0_potential_pairs.txt")
 
 
 
