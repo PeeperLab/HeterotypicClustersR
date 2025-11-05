16f82e7b021ab327f1ff0fc077bca8f9aed4ec14
AustinGeorgeNKI <a.george@nki.nl>
2025-11-05 16:20:38 +0100

    Update s2_AUC_signatures_of_interest.R
    updated with tables associated with plots

--- Scripts/8.Tumor_Analysis/s2_AUC_signatures_of_interest.R
+++ Scripts/8.Tumor_Analysis/s2_AUC_signatures_of_interest.R
@@ -21,7 +21,7 @@ set.seed(150799)
 fig_path = "Results_tumor_focused_analysis"
 if(!dir.exists(fig_path)) dir.create(fig_path)
 
-five_tum=readRDS("Tumor_Annotated.Rds")
+five_tum=readRDS("Results_tumor/Tumor_Annotated.Rds")
 DefaultAssay(five_tum)="RNA"
 
 five_tum=subset(five_tum, annotated_clusters != "Low Gene Count Cells")
@@ -29,7 +29,7 @@ five_tum=subset(five_tum, annotated_clusters != "Low Gene Count Cells")
 exprMatrix= five_tum@assays$RNA@counts
 exprMatrix <- as(exprMatrix, "dgCMatrix")
 
-gene.set=fgsea::gmtPathways("c2.cp.v7.5.1.symbols.gmt") 
+gene.set=fgsea::gmtPathways("extdata/signatures_tcells/c2.cp.v7.5.1.symbols.gmt") 
 gene.set=gene.set[names(gene.set) %in% c(
   "REACTOME_ANTIGEN_PRESENTATION_FOLDING_ASSEMBLY_AND_PEPTIDE_LOADING_OF_CLASS_I_MHC",
   "REACTOME_INTERFERON_ALPHA_BETA_SIGNALING",
@@ -48,7 +48,7 @@ auc_numbers$cell_id=rownames(auc_numbers)
 meta=five_tum@meta.data
 meta$cell_id=gsub("-",".",rownames(meta))
 auc_numbers_merged=merge(auc_numbers, meta, by="cell_id")
-saveRDS(auc_numbers_merged,"auc_numbers_merged_filt.rds")
+saveRDS(auc_numbers_merged,"Results_tumor_focused_analysis/auc_numbers_merged_filt.rds")
 
 setDT(auc_numbers_merged)
 auc_numbers_merged[, sample_id:= factor(sample_id, levels=c("SG_Singlets", "DB_Tumor_Tcell"))]
@@ -57,37 +57,25 @@ auc_avg=auc_numbers_merged[, lapply(.SD, mean), by=c("patient_id", "sample_id"),
 
 ggplot(auc_numbers_merged, aes(patient_id, fill=sample_id,y = PID_HIF1_TFPATHWAY))+
   geom_boxplot(alpha=1, width=1)+
-  stat_compare_means(size=4, method = "wilcox.test", label = "p.signif", 
-                     symnum.args = list(cutpoints = c(0, 0.0001, 0.001, 0.01, 0.05, 1), 
-                                        symbols = c("****", "***", "**", "*", "ns")))+
+  stat_compare_means(size = 4, method = "wilcox.test", 
+                     label = "p.format")+
   ylab("AUC score: HIF1 TF pathway")+
   theme_bw()+theme(panel.grid.major = element_blank(), 
                    panel.grid.minor = element_blank())+
   scale_fill_manual(values=c(DB_Tumor_Tcell="#825d9e",
                              SG_Singlets = "#7cd3f7"))+xlab("")
-ggsave("Results_tumor_focused_analysis/HIF_score_per_pat.pdf", height=4.5, width=5)
+ggsave("Results_tumor_focused_analysis/HIF_score_per_pat.pdf", height=4.5, width=8)
+
+write.csv(auc_numbers_merged,"Results_tumor_focused_analysis/Tumor_AUC_Specific_Pathways.csv")
 
 ggplot(auc_numbers_merged, aes(patient_id, fill=sample_id,y = REACTOME_ANTIGEN_PRESENTATION_FOLDING_ASSEMBLY_AND_PEPTIDE_LOADING_OF_CLASS_I_MHC))+
   geom_boxplot(alpha=1, width=1)+
-  stat_compare_means(size=4, method = "wilcox.test", label = "p.signif", 
-                     symnum.args = list(cutpoints = c(0, 0.0001, 0.001, 0.01, 0.05, 1), 
-                                        symbols = c("****", "***", "**", "*", "ns")))+
+  stat_compare_means(size = 4, method = "wilcox.test", 
+                     label = "p.format")+
   ylab("AUC score: Antigen presentation,\n folding assembly and\n peptide loading of class I MHC")+
   theme_bw()+theme( panel.grid.major = element_blank(), 
                    panel.grid.minor = element_blank())+
   scale_fill_manual(values=c(DB_Tumor_Tcell="#825d9e",
                              SG_Singlets = "#7cd3f7"))+xlab("")
-ggsave("Results_tumor_focused_analysis/MHC_score_per_pat.pdf", height=4.5, width=5)
-
-ggplot(auc_numbers_merged, aes(patient_id, fill=sample_id,y = REACTOME_INTERFERON_ALPHA_BETA_SIGNALING))+
-  geom_boxplot(alpha=1, width=1)+
-  stat_compare_means(size=4, method = "wilcox.test", label = "p.signif", 
-                     symnum.args = list(cutpoints = c(0, 0.0001, 0.001, 0.01, 0.05, 1), 
-                                        symbols = c("****", "***", "**", "*", "ns")))+
-  ylab("AUC score: IFN A/B pathway")+
-  theme_bw()+theme(panel.grid.major = element_blank(), 
-                   panel.grid.minor = element_blank())+
-  scale_fill_manual(values=c(DB_Tumor_Tcell="#825d9e",
-                             SG_Singlets = "#7cd3f7"))+xlab("")
-ggsave("Results_tumor_focused_analysis/IFNAB_score_per_pat.pdf", height=4.5, width=5)
+ggsave("Results_tumor_focused_analysis/MHC_score_per_pat.pdf", height=4.5, width=8)
 
